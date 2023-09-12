6dccaf643110b9421732bda28aca505a71594d16
Tran Dinh Phu <56244176+cdf144@users.noreply.github.com>
2023-09-12 15:48:56 +0700

    Ignored .class files, removed oop package

--- src/main/java/oop/Main.java
+++
@@ -1,9 +0,0 @@
-package oop;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
