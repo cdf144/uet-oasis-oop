8cd942cd53028f88608a5b97bb31e4a2aecb4caf
Tran Dinh Phu <trandinhphu@gmail.com>
2023-09-15 12:53:43 +0700

    java.Scanner test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce your name, average grade and age");
+
+        String userName = sc.nextLine();
+        Double averageGrade = sc.nextDouble();
+        Integer age = sc.nextInt();
+
+        System.out.printf("%-25s%-5.2f %-5d", userName, averageGrade, age);
     }
 }
