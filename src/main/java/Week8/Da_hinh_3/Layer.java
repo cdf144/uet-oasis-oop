459d8ace6f5695f7800def5b07078cc80ab1dcb5
Tran Dinh Phu <trandinhphu@gmail.com>
2023-10-06 00:43:06 +0700

    Graphic Shape demo?

---
+++ src/main/java/Week8/Da_hinh_3/Layer.java
@@ -0,0 +1,55 @@
+package Week8.Da_hinh_3;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class Layer {
+    private List<Shape> shapes;
+
+    public Layer() {
+        shapes = new ArrayList<>();
+    }
+
+    public void addShape(Shape shape) {
+        shapes.add(shape);
+    }
+
+    public List<Shape> getShapes() {
+        return shapes;
+    }
+
+    /**
+     * Get info of layer.
+     *
+     * @return layer string info
+     */
+    public String getInfo() {
+        StringBuilder s = new StringBuilder();
+        s.append("Layer of crazy shapes:");
+        for (Shape shape : shapes) {
+            s.append("\n").append(shape.toString());
+        }
+        return s.toString();
+    }
+
+    public void removeCircles() {
+        shapes.removeIf(shape -> shape instanceof Circle);
+    }
+
+    /**
+     * Remove duplicates from shape list.
+     */
+    public void removeDuplicates() {
+        LinkedHashSet<Shape> shapeSet = new LinkedHashSet<>(shapes);
+        this.shapes = new ArrayList<>(shapeSet);
+    }
+
+    public static void main(String[] args) {
+        Point point = new Point(5.0, 8.0);
+        Shape s1 = new Square(point, 4.0, "", true);
+        System.out.println(s1);
+        Rectangle s2 = new Square(point, 2.5, "TRUE", true);
+        System.out.println(s2);
+    }
+}
---
+++ src/main/java/Week8/Da_hinh_3/Point.java
@@ -0,0 +1,105 @@
+package Week8.Da_hinh_3;
+
+public class Point {
+    private double pointX;
+    private double pointY;
+
+    /**
+     * Point constructor 1.
+     */
+    public Point() {
+        pointX = 0.0;
+        pointY = 0.0;
+    }
+
+    /**
+     * Point constructor 2.
+     *
+     * @param pointX pointX
+     * @param pointY pointY
+     */
+    public Point(double pointX, double pointY) {
+        this.pointX = pointX;
+        this.pointY = pointY;
+    }
+
+    public double getPointX() {
+        return pointX;
+    }
+
+    public void setPointX(double pointX) {
+        this.pointX = pointX;
+    }
+
+    public double getPointY() {
+        return pointY;
+    }
+
+    public void setPointY(double pointY) {
+        this.pointY = pointY;
+    }
+
+    /**
+     * Calculate distance between 2 points.
+     *
+     * @param newPoint another point
+     * @return distance
+     */
+    public double distance(Point newPoint) {
+        if (newPoint != null) {
+            return Math.sqrt((this.pointX - newPoint.getPointX())
+                    * (this.pointX - newPoint.getPointX())
+                    + (this.pointY - newPoint.getPointY())
+                    * (this.pointY - newPoint.getPointY())
+            );
+        } else {
+            return Math.sqrt(this.pointX * this.pointX
+                    + this.pointY * this.pointY);
+        }
+    }
+
+    /**
+     * Boolean if 2 points are equal.
+     *
+     * @param o another object
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Point) {
+            Point another = (Point) o;
+            return Math.abs(this.pointX - another.getPointX())
+                    <= 0.001
+                    && Math.abs(this.pointY - another.getPointY())
+                    <= 0.001;
+        }
+        return false;
+    }
+
+    /**
+     * Point toString.
+     *
+     * @return Point string
+     */
+    @Override
+    public String toString() {
+        return String.format("(%.1f,%.1f)",
+                this.pointX,
+                this.pointY
+        );
+    }
+
+    /**
+     * Point hashcode.
+     *
+     * @return hashcode
+     */
+    @Override
+    public int hashCode() {
+        int hashCode = 1;
+        hashCode += (int) Math.round(this.pointX);
+        hashCode += (int) Math.round(this.pointY);
+        hashCode += (int) Math.round(this.pointX * this.pointY);
+        return hashCode;
+    }
+}
