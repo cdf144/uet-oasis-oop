dc683d8afd6293f3c5135c016707bc7957e56568
Tran Dinh Phu <56244176+cdf144@users.noreply.github.com>
2023-10-27 22:28:01 +0700

    Updated BankingSystem and its Test. Added I/O Utils

--- src/main/java/Week13/Banking_System/Bank.java
+++ src/main/java/Week13/Banking_System/Bank.java
@@ -32,19 +32,16 @@ public class Bank {
      *
      * @param inputStream input stream
      */
-    public void readCustomerList(InputStream inputStream) {
-        BufferedReader bufferedReader
-                = new BufferedReader(new InputStreamReader(inputStream));
+    public void readCustomerList(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         String readLine;
-        try {
-            readLine = bufferedReader.readLine();
-        } catch (IOException e) {
-            return;
-        }
+        // Loop for reading Customer info
+        readLine = bufferedReader.readLine();
         while (readLine != null) {
             String[] partsCustomer = readLine.split(" ");
             StringBuilder customerName = new StringBuilder();
             long customerId = 0;
+
             for (String part : partsCustomer) {
                 char firstChar = part.charAt(0);
                 if (!Character.isDigit(firstChar)) {
@@ -57,18 +54,17 @@ public class Bank {
                     customerId = Long.parseLong(part);
                 }
             }
+
             Customer newCustomer = new Customer(customerId, customerName.toString());
-            try {
-                readLine = bufferedReader.readLine();
-            } catch (IOException e) {
-                return;
-            }
-            while (readLine != null
-                    && Character.isDigit(readLine.charAt(0))) {
+
+            // Loop for reading Customer's Account(s) info
+            readLine = bufferedReader.readLine();
+            while (readLine != null && Character.isDigit(readLine.charAt(0))) {
                 String[] partsAccount = readLine.split(" ");
                 long accountId = Long.parseLong(partsAccount[0]);
                 String operation = partsAccount[1];
                 double balance = Double.parseDouble(partsAccount[2]);
+
                 try {
                     if (Objects.equals(operation, Account.CHECKING)) {
                         newCustomer.addAccount(new CheckingAccount(accountId, balance));
@@ -78,29 +74,30 @@ public class Bank {
                         throw new IllegalArgumentException();
                     }
                 } catch (IllegalArgumentException e) {
+                    System.err.println(e);
                     return;
                 }
-                try {
-                    readLine = bufferedReader.readLine();
-                } catch (IOException e) {
-                    return;
-                }
+
+                readLine = bufferedReader.readLine();
             }
+
+            // Check if Customer already exists in CustomerList
+            // If so, add the Account(s) to the existing Customer
             int i = 0;
-            while (i < this.customerList.size()) {
-                if (this.customerList.get(i).getIdNumber()
-                        == customerId) {
-                    for (int j = 0;
-                         j < newCustomer.getAccountList().size(); j++) {
-                        this.customerList.get(i)
-                                .addAccount(newCustomer.getAccountList().get(j));
+            while (i < customerList.size()) {
+                if (customerList.get(i).getIdNumber() == customerId) {
+                    for (int j = 0; j < newCustomer.getAccountList().size(); j++) {
+                        customerList.get(i).addAccount(newCustomer.getAccountList().get(j));
                     }
                     break;
                 }
                 i++;
             }
-            if (i == this.customerList.size()) {
-                this.customerList.add(newCustomer);
+
+            // If Customer does not exist in CustomerList
+            // Add the Customer along with their account(s)
+            if (i == customerList.size()) {
+                customerList.add(newCustomer);
             }
         }
     }
@@ -117,13 +114,12 @@ public class Bank {
     public String getCustomersInfoByIdOrder() {
         if (this.isNotEmpty()) {
             StringBuilder customersInfoByIdOrder = new StringBuilder();
-            this.customerList.sort(new IdComparator());
-            for (Customer customer : this.customerList) {
+            customerList.sort(new IdComparator());
+            for (Customer customer : customerList) {
                 if (customersInfoByIdOrder.length() == 0) {
                     customersInfoByIdOrder.append(customer.getCustomerInfo());
                 } else {
-                    customersInfoByIdOrder.append("\n")
-                            .append(customer.getCustomerInfo());
+                    customersInfoByIdOrder.append("\n").append(customer.getCustomerInfo());
                 }
             }
             return customersInfoByIdOrder.toString();
@@ -139,17 +135,16 @@ public class Bank {
      */
     public String getCustomersInfoByNameOrder() {
         if (this.isNotEmpty()) {
-            StringBuilder customersInfoByIdOrder = new StringBuilder();
-            this.customerList.sort(new NameComparator());
-            for (Customer customer : this.customerList) {
-                if (customersInfoByIdOrder.length() == 0) {
-                    customersInfoByIdOrder.append(customer.getCustomerInfo());
+            StringBuilder customersInfoByNameOrder = new StringBuilder();
+            customerList.sort(new NameComparator());
+            for (Customer customer : customerList) {
+                if (customersInfoByNameOrder.length() == 0) {
+                    customersInfoByNameOrder.append(customer.getCustomerInfo());
                 } else {
-                    customersInfoByIdOrder.append("\n")
-                            .append(customer.getCustomerInfo());
+                    customersInfoByNameOrder.append("\n").append(customer.getCustomerInfo());
                 }
             }
-            return customersInfoByIdOrder.toString();
+            return customersInfoByNameOrder.toString();
         } else {
             return "";
         }
--- src/test/java/Week11/Ngoai_le_1/ExpressionTest.java
+++ src/test/java/Week11/Ngoai_le_1/ExpressionTest.java
@@ -1,19 +1,23 @@
 package Week11.Ngoai_le_1;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ExpressionTest {
-    /**
-     * Driver test.
-     *
-     * @param args arguments
-     */
-    public static void main(String[] args) {
+    @Test
+    public void testExpression() {
         Square square = new Square(new Numeral(10));
         Subtraction subtraction = new Subtraction(square, new Numeral(3));
         Multiplication multiplication = new Multiplication(new Numeral(4), new Numeral(3));
+
         Addition additionAll = new Addition(subtraction, multiplication);
         Square squareAll = new Square(additionAll);
 
-        System.out.println(squareAll);
-        System.out.println(squareAll.evaluate());
+        String expected = "((((10) ^ 2 + -3) + (4 * 3))) ^ 2";
+        assertEquals(expected, squareAll.toString());
+
+        final double DELTA = 0.0000001;
+        assertEquals(11881.0, squareAll.evaluate(), DELTA);
     }
 }
