98ea92f4bb5e4a0ced0884ec1f043f8c32c2959f
Tran Dinh Phu <56244176+cdf144@users.noreply.github.com>
2023-11-09 15:57:43 +0700

    RegEx correction to properly pass tests

--- src/main/java/Week13/Phan_tich_chuong_trinh/Week10.java
+++ src/main/java/Week13/Phan_tich_chuong_trinh/Week10.java
@@ -4,17 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * HEADS UP: Originally this code only passes 7/8 tests. I managed to make
- * it pass the last test using a trick, which means there is something wrong
- * in my STATIC_METHOD_PATTERN. If you have found a correct RegEx or have any
- * clue why my RegEx is wrong, please consider opening a GitHub Issue to notify
- * me, or better yet, change it yourself by opening a Pull Request.
- */
 public class Week10 {
     private static final Pattern PACKAGE_PATTERN = Pattern.compile(
             "^package\\s(.*);$",
@@ -33,9 +25,10 @@ public class Week10 {
     );
 
     private static final Pattern STATIC_METHOD_PATTERN = Pattern.compile(
-            "^(?:\\s{2})+(?:(?:abstract|public|private|protected)\\s+)*"
+            "^(?<!\\s{0,20}/\\*\\n)" // Negative lookbehind
+                    + "(?:\\s{2})+(?:(?:abstract|public|private|protected)\\s+)*"
                     + "static\\s+(?:final\\s+)?[\\w<>,.?\\[\\]\\s]+\\s+(\\w+)"
-                    + "\\s*\\(([\\w<>,.?\\[\\]\\s]*)\\)\\s+\\{",
+                    + "\\s*\\(([\\w<>,.?\\[\\]\\s]*)\\)\\s*\\{",
             Pattern.MULTILINE
     );
 
@@ -97,13 +90,6 @@ public class Week10 {
         while (matcher.find()) {
             String methodName = matcher.group(1);
 
-            // trick to pass the last test
-            if (Objects.equals(methodName, "randomIntGreaterThan")) {
-                continue;
-            } else if (Objects.equals(methodName, "randomDoubleGreaterThan")) {
-                allMethods.add("randomDouble(double,double)");
-            }
-
             String allParams = matcher.group(2);
             StringBuilder allTypes = new StringBuilder();
             allTypes.append("(");
@@ -126,11 +112,6 @@ public class Week10 {
 
             allTypes.append(")");
             allMethods.add(methodName + allTypes.toString());
-
-            // trick to pass the last test
-            if (Objects.equals(methodName, "randomPositiveLong")) {
-                allMethods.add("randomNegativeLong()");
-            }
         }
 
         return allMethods;
